a7f49d2f2bfaa57393c04862f2b1acd32c5c81ed
syahrulutomo <irullvandalis@gmail.com>
2018-10-17 18:17:38 +0700

    swagger2

---
+++ src/main/java/com/kaledo/backend/kaledobackend/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.kaledo.backend.kaledobackend;
+
+import io.swagger.models.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kaledo.backend.kaledobackend.controller"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaData());
+                }
+     private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Kaledo Backend",
+                "1.0",
+                "Terms of service",
+                "Build by Kaledo Team ",
+               "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
+    }
+}
+    
