9456377e36cb6fbd8df1a25b8b0712ddf85c90e1
syahrulutomo <irullvandalis@gmail.com>
2018-11-01 21:56:02 +0700

    fix recipe API and add howto API

--- src/main/java/com/kaledo/backend/kaledobackend/entity/Recipe.java
+++ src/main/java/com/kaledo/backend/kaledobackend/entity/Recipe.java
@@ -122,5 +122,14 @@ public class Recipe {
     public void setTime(String time) {
         this.time = time;
     }
- 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
 }
